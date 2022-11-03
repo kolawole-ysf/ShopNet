1a3cc1bdc8bed78bf9d3cfdbfcda4b0bce1c1939
kolawole-ysf <111007555+kolawole-ysf@users.noreply.github.com>
2022-11-03 18:21:18 +0000

    update

---
+++ app.js
@@ -0,0 +1,6 @@
+//toggle button
+const toggleBtn=document.querySelector('.toggle');
+
+toggleBtn.addEventListener('click',()=>{
+        document.querySelector('.navbar').classList.toggle('active');
+})
