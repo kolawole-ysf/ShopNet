988540298185fb033025fe16a8edaa7f73b1711a
kolawole-ysf <111007555+kolawole-ysf@users.noreply.github.com>
2022-11-06 21:22:23 +0000

    update

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 //toggle button
-const toggleBtn=document.querySelector('.toggle');
+// const toggleBtn=document.querySelector('.toggle');
 
-toggleBtn.addEventListener('click',()=>{
-        document.querySelector('.navbar').classList.toggle('active');
+// toggleBtn.addEventListener('click',()=>{
+//         document.querySelector('.navbar').classList.toggle('active');
+// })
+
+const toggleBtn=document.querySelectorAll('.toggle');
+const nav=document.querySelector('.navbar');
+
+toggleBtn.forEach(btn=>{
+        btn.addEventListener('click', ()=>{
+                if(btn.classList.contains('open')){
+                        nav.classList.add('active');
+                }
+                else{
+                        nav.classList.remove('active');
+                }
+        })
 })
