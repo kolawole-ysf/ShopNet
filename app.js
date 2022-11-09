a64f86994bbd3c362c14523a37ed301e9da3ae53
kolawole-ysf <111007555+kolawole-ysf@users.noreply.github.com>
2022-11-09 20:06:11 +0000

    update

--- app.js
+++ app.js
@@ -1,10 +1,4 @@
-//toggle button
-// const toggleBtn=document.querySelector('.toggle');
-
-// toggleBtn.addEventListener('click',()=>{
-//         document.querySelector('.navbar').classList.toggle('active');
-// })
-
+//Toggle buttons
 const toggleBtn=document.querySelectorAll('.toggle');
 const nav=document.querySelector('.navbar');
 
@@ -45,14 +39,62 @@ cartBtns.forEach(cartBtn=>{
  }
 cartContent();
 
+//record product details in local storage
+const cartButtons=document.querySelectorAll('.cart');
 
+let products=[];
+cartButtons.forEach(button=>{
+        button.addEventListener('click',(e)=>{
+        const product={
+                image: e.target.parentElement.children[0].src,
+                name: e.target.parentElement.children[1].children[1].innerText,
+                quantity: 1,
+                price: parseInt(e.target.parentElement.children[1].children[3].innerText),
+                totalPrice: parseInt(e.target.parentElement.children[1].children[3].innerText)
+        }
+        storeProduct(product);
+        })
+})
 
-
-
- //cart Records
- const proCard=document.querySelector('.product-card');
- const proImage=document.querySelector('.product-img');
- const proName=document.querySelector('.product-details h5');
- const proPrice=document.querySelector('.product-details h4');
- const cartContainer=document.querySelector('.cart-container');
+function storeProduct(product){
+        let cartItem= JSON.parse(localStorage.getItem('prdInCart'));
+        if(cartItem===null){
+                products.push(product);
+                localStorage.setItem('prdInCart',JSON.stringify(products));
+        }else{
+                cartItem.forEach(item=>{
+                        if(product.image==item.image){
+                                product.quantity = item.quantity +=1;
+                                product.totalPrice=product.price*product.quantity;
+                                
+                        }else{
+                                products.push(item)
+                        }
+                })
+                products.push(product);
+        }
+        localStorage.setItem('prdInCart',JSON.stringify(products));
+        window.location.reload();
+}
+ //display local storage content
+ function displayCart(){
+        let cartCard='';
+        let cartItem= JSON.parse(localStorage.getItem('prdInCart'));
+        cartItem.forEach(item=>{
+                cartCard+=`
+                        <div class="cart-card">
+                        <img src="${item.image}" alt="">
+                           <ul class="collection">
+                                <li class="item">Product:<span>${item.name}</span></li>
+                                <li class="item">QTY:<span>${item.quantity}</span></li>
+                                <li class="item">Price:<span>${item.price}</span></li>
+                                <li class="item">Total Price:<span>${item.totalPrice}</span></li>
+                           </ul>
+                        </div>
+                
+                `;  
+        })
+        document.querySelector('.cart-container').innerHTML=cartCard; 
+ }
+ displayCart();
  
